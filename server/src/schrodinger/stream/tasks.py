4415a5b2f09498805f36b38423286582b66bad35
Benjamin Fovet <b.fovet@gmail.com>
2026-01-01 15:52:45 +0100

    refactor(stream): improve code for fetching frames

--- server/src/schrodinger/stream/tasks.py
+++ server/src/schrodinger/stream/tasks.py
@@ -1,11 +1,14 @@
 import pickle
 import subprocess
 import time
+from dataclasses import dataclass
 from datetime import datetime
+from typing import IO
 
 import numpy as np
 import structlog
 from celery.utils.log import get_task_logger
+from redis import Redis
 
 from schrodinger.celery import celery
 from schrodinger.logging import Logger
@@ -14,12 +17,19 @@ from schrodinger.worker.redis import RedisTask
 log: Logger = structlog.wrap_logger(get_task_logger(__name__))
 
 
-STREAM_NAME = "frame_stream"
-CONSUMER_GROUP = "detection_group"
-CONSUMER_NAME = "detector_1"
+STREAM_NAME = "frames"
 
 
-def get_stream_resolution(rtsp_url: str) -> tuple[int, int]:
+@dataclass
+class FrameDimension:
+    width: int
+    height: int
+
+    def frame_size(self) -> int:
+        return self.width * self.height * 3
+
+
+def get_stream_resolution(rtsp_url: str) -> FrameDimension:
     # fmt: off
     probe_cmd = [
         "ffprobe",
@@ -43,36 +53,63 @@ def get_stream_resolution(rtsp_url: str) -> tuple[int, int]:
         raise RuntimeError("Could not detect resolution")
 
     width, height = map(int, probe_result.stdout.strip().split(","))
-    log.info("Detected stream resolution", width=width, height=height)
+    frame_dimension = FrameDimension(width, height)
+    log.info("Detected stream resolution", frame_dimensions=frame_dimension)
 
-    return width, height
+    return frame_dimension
+
+
+def publish_single_frame(
+    frame_data: IO[str], frame_dim: FrameDimension, redis: Redis
+) -> None:
+    raw_frame = frame_data.read(frame_dim.frame_size())
+    frame = np.frombuffer(raw_frame, dtype=np.uint8).reshape(
+        (frame_dim.height, frame_dim.width, 3)
+    )
+
+    redis.xadd(
+        STREAM_NAME,
+        {
+            "frame": pickle.dumps(frame),
+            "timestamp": datetime.now().timestamp(),
+        },
+        maxlen=1,
+    )
+
+
+def try_killing_process(process: subprocess.Popen | None) -> None:
+    if "process" in locals() and process is not None:
+        process.kill()
 
 
 @celery.task(name="fetch_frames", base=RedisTask, bind=True)
 def fetch_frames(self, rtsp_url: str):
     process: subprocess.Popen | None = None
-    width, height = get_stream_resolution(rtsp_url)
+
+    try:
+        frame_dim = get_stream_resolution(rtsp_url)
+    except RuntimeError:
+        frame_dim = FrameDimension(1920, 1080)
+
+    # fmt: off
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-rtsp_transport", "tcp",
+        "-fflags", "nobuffer+discardcorrupt",
+        "-flags", "low_delay",
+        "-analyzeduration", "1",
+        "-probesize", "32",
+        "-i", rtsp_url,
+        "-f", "rawvideo",
+        "-pix_fmt", "bgr24",
+        "-an",  # Disable audio
+        "-vf", "fps=10",  # Limit to 10 fps to reduce processing load
+        "-",
+    ]
+    # fmt: on
+
     while True:
         try:
-            frame_size = width * height * 3
-
-            # fmt: off
-            ffmpeg_cmd = [
-                "ffmpeg",
-                "-rtsp_transport", "tcp",
-                "-fflags", "nobuffer+discardcorrupt",
-                "-flags", "low_delay",
-                "-analyzeduration", "1",
-                "-probesize", "32",
-                "-i", rtsp_url,
-                "-f", "rawvideo",
-                "-pix_fmt", "bgr24",
-                "-an",  # Disable audio
-                "-vf", "fps=10",  # Limit to 10 fps to reduce processing load
-                "-",
-            ]
-            # fmt: on
-
             process = subprocess.Popen(
                 ffmpeg_cmd,
                 stdout=subprocess.PIPE,
@@ -83,38 +120,20 @@ def fetch_frames(self, rtsp_url: str):
             log.info("Started FFmpeg capture")
 
             while True:
-                raw_frame = process.stdout.read(frame_size)
-
-                if len(raw_frame) != frame_size:
+                if process.stdout is None:
                     log.info("FFmpeg stream ended")
                     break
 
-                frame = np.frombuffer(raw_frame, dtype=np.uint8)
-                frame = frame.reshape((height, width, 3))
+                publish_single_frame(process.stdout, frame_dim, self.redis)
 
-                frame_time = datetime.now()
-                frame_data = {"timestamp": frame_time.timestamp(), "frame": frame}
-
-                self.redis.xadd(
-                    STREAM_NAME,
-                    {
-                        "frame_data": pickle.dumps(frame_data),
-                        "timestamp": frame_time.timestamp(),
-                    },
-                    maxlen=1,
-                )
-
-            process.stdout.close()
-            process.stderr.close()
+            # process.stdout.close()
+            # process.stderr.close()
             process.terminate()
             process.wait(timeout=5)
-
         except subprocess.TimeoutExpired:
             log.warning("FFmpeg process timeout")
-            if "process" in locals():
-                process.kill()
+            try_killing_process(process)
         except Exception as e:
             log.error("Error in FFmpeg capture", error=e)
-            if "process" in locals():
-                process.kill()
+            try_killing_process(process)
             time.sleep(2)
